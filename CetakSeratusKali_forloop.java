e4b3406ea334f08b58be1a175b58048d240696d9
hillaryisfalanajune <114393979+hillaryisfalanajune@users.noreply.github.com>
2022-11-16 22:24:10 +0700

    Add files via upload

---
+++ CetakSeratusKali_forloop.java
@@ -0,0 +1,9 @@
+package hillaryisfalanajune_2101092008_modul6;
+
+public class CetakSeratusKali_forloop {
+ public static void main(String[] args)
+ {int i;
+    for (i=1;i<=100;i++)
+    {System.out.println(i+ "HILLARY ISFALANA JUNE");}
+ }   
+}
